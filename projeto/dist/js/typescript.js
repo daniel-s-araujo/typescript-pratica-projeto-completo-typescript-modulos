b238aa87f7de3c709e654757e11b695d01ed0fc0
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-11 13:38:40 -0300

    Arrays e type alias

--- projeto/dist/js/typescript.js
+++ projeto/dist/js/typescript.js
@@ -3,3 +3,10 @@ let nome = "";
 let isPago = false;
 let qualquer = "";
 qualquer = 22;
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0,
+};
--- projeto/src/typescript.ts
+++ projeto/src/typescript.ts
@@ -1,6 +1,20 @@
 let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
-
 let qualquer: any = "";
 qualquer = 22;
+
+const lista: number[] = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+
+type Transacao = {
+  tipoTransacao: string;
+  data: Date;
+  valor: number;
+};
+
+const novaTransacao: Transacao = {
+  tipoTransacao: "",
+  data: new Date(),
+  valor: 0,
+};
